960bec6c07cc41400692caa4a87135cabca42f66
Chisom Laura <102640478+ChisomLaura@users.noreply.github.com>
2022-06-15 19:48:24 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log("Hi!");
+//console.log("Hi!")
+
+var mainTitle = document.querySelector("h1");
+console.log(mainTitle);
+mainTitle.innerText = "Yayy, Javascript!!";
+console.log(mainTitle);
+
+var body = document.querySelector("body");
+console.log(body);
+body.style.backgroundColor = "#FEF2F1";
+body.style.color = "black";
+console.log(body);
